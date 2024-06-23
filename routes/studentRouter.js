6e24be2be85369bfa00b6ca17975ee84754278c7
amir miftah <amirmiftah555@gmail.com>
2024-06-23 14:10:10 +0300

    final commit

--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -17,13 +17,13 @@ import {
 import { authenticateUser } from "../middleware/authMiddleware.js";
 import rateLimiter from "express-rate-limit";
 
-const apiLimiter = rateLimiter({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 15,
-  message: { msg: "IP rate limit exceeded, retry in 15 minutes." },
-});
-router.post("/register", apiLimiter, validateStudentInput, register);
-router.post("/login", apiLimiter, validateStudentInput, login);
+// const apiLimiter = rateLimiter({
+//   windowMs: 15 * 60 * 1000, // 15 minutes
+//   max: 15,
+//   message: { msg: "IP rate limit exceeded, retry in 15 minutes." },
+// });
+router.post("/register", validateStudentInput, register);
+router.post("/login", validateStudentInput, login);
 router.get("/allStudents", authenticateUser, getAllStudents);
 router.post("/prevote/:id", authenticateUser, validateIdParam, preliminaryVote);
 router.get("/candidates", authenticateUser, getCandidates);
