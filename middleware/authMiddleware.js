df5f238ac5a9a098bd3d6cef843b09d618c96479
amir miftah <amirmiftah555@gmail.com>
2024-06-23 17:21:37 +0300

    modified some files

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,6 +2,7 @@ import { UnauthenticatedError } from "../errors/customError.js";
 import { verifyJWT } from "../utils/tokenUtils.js";
 export const authenticateUser = (req, res, next) => {
   const { token } = req.cookies;
+
   if (!token) {
     throw new UnauthenticatedError("authentication invalid");
   }
